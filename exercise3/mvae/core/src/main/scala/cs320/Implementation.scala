ec16f3c4ac355e239db91043a9b748a194883b83
Jinho Choi <choijinho817@gmail.com>
2020-09-13 21:55:26 +0900

    stuck while implementing interp

--- exercise3/mvae/core/src/main/scala/cs320/Implementation.scala
+++ exercise3/mvae/core/src/main/scala/cs320/Implementation.scala
@@ -11,9 +11,19 @@ object Implementation extends Template {
   ): List[Int] = ls match {
     case Nil => Nil
     case l :: rest =>
-      def f(r: Int): Int = ???
+      def f(r: Int): Int = {
+        op(l, r)
+      }
       rs.map(f) ++ binOp(op, rest, rs)
   }
 
-  def interp(expr: Expr): List[Int] = ???
+  def interp(expr: Expr): List[Int] = 
+    def add(num1:Int, num2:Int):Int = num1+num2
+    def sub(num1:Int, num2:Int):Int = num1-num2
+    def val
+    expr match{
+      case Add(l, r) => binOp(add, interp(l), interp(r))
+      case Num(v) => v 
+      case Id(id) => 
+  }
 }
