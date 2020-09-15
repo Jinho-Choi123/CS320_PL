9e77ea83fe2d7519fc3b87c0f1956b693dc9963d
Jinho Choi <choijinho817@gmail.com>
2020-09-16 03:20:47 +0900

    finish exercise3

--- exercise3/mvae/core/src/main/scala/cs320/Implementation.scala
+++ exercise3/mvae/core/src/main/scala/cs320/Implementation.scala
@@ -9,21 +9,32 @@ object Implementation extends Template {
     ls: List[Int],
     rs: List[Int]
   ): List[Int] = ls match {
-    case Nil => Nil
+    case Nil => List()
     case l :: rest =>
-      def f(r: Int): Int = {
+      {
+        def f(r: Int): Int = {
         op(l, r)
+        }
+        rs.map(f) ++ binOp(op, rest, rs)
       }
-      rs.map(f) ++ binOp(op, rest, rs)
   }
 
-  def interp(expr: Expr): List[Int] = 
-    def add(num1:Int, num2:Int):Int = num1+num2
-    def sub(num1:Int, num2:Int):Int = num1-num2
-    def val
-    expr match{
-      case Add(l, r) => binOp(add, interp(l), interp(r))
-      case Num(v) => v 
-      case Id(id) => 
+  def interp(expr: Expr): List[Int] = {
+    def Map_find(input_map: Map[String, List[Int]], key:String) = {
+      if (input_map.contains(key)) input_map(key)
+      else error("free identifier")
+    }
+    def min(x:Int, y:Int):Int = if (x>y) y else x
+    def max(x:Int, y:Int):Int = if (x>y) x else y
+    def aux(expr: Expr, env: Map[String, List[Int]]): List[Int] = expr match {
+        case Add(l, r) => binOp((x:Int,y:Int)=>x+y, aux(l, env), aux(r, env))
+        case Sub(l, r) => binOp((x:Int,y:Int)=>x-y, aux(l, env), aux(r, env))
+        case Val(name, expression, body) => aux(body, env + (name -> aux(expression, env)))
+        case Id(id) => Map_find(env,id)
+        case Num(values) => values
+        case Min(left, mid, right) => binOp(min, binOp(min, aux(left,env), aux(mid,env)), aux(right,env))
+        case Max(left, mid, right) => binOp(max, binOp(max, aux(left,env), aux(mid,env)), aux(right,env))
+      }
+      aux(expr, Map())
   }
 }
